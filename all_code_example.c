73f79bf187210d927f4659024df1f0484b59b1c5
hoangthien <a11718thien@gmail.com>
2023-12-18 15:49:04 +0700

    add typedef

--- all_code_example.c
+++ all_code_example.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define MY_DEFINE 2
+
+#ifdef MY_DEFINE 
+    #if MY_DEFINE == 1
 // Handler definition
 typedef void (*ButtonClickHandlerFunc_t)();
 
@@ -17,37 +21,19 @@ void HandleButtonClickB() {
     printf("Button B clicked!\n");
 }
 
-// Function to set the click handler for a buttons
+// Set the click handler for a buttons
 void SetButtonClickHandler(Button* button, ButtonClickHandlerFunc_t clickHandler) {
     button->clickHandler = clickHandler;
 }
 
-// Function to simulate button click event
+// Simulate button 
 void SimulateButtonClick(Button* button) {
     if (button->clickHandler != NULL) {
         button->clickHandler();
     }
 }
 
-int main() {
-    // Create Button A
-    Button buttonA;
-    SetButtonClickHandler(&buttonA, HandleButtonClickA);
-    SimulateButtonClick(&buttonA);
-
-    // Create Button B
-    Button buttonB;
-    SetButtonClickHandler(&buttonB, HandleButtonClickB);
-    SimulateButtonClick(&buttonB);
-
-    return 0;
-}
-
-
-//=============================================================================================================
-
-#include <stdio.h>
-
+    #elif MY_DEFINE == 2
 // Handler definition
 typedef void (*EventHandlerFunc_t)();
 
@@ -90,54 +76,69 @@ void SimulateDoubleClick(Event* event) {
         event->doubleClickHandler();
     }
 }
+    #endif
+#endif
 
 int main() {
-    // Create an event
-    Event myEvent;
+    #if MY_DEFINE == 1
+    // buttonA click
+        Button buttonA;
+        SetButtonClickHandler(&buttonA, HandleButtonClickA);
+        SimulateButtonClick(&buttonA);
+
+        Button buttonB;
+        SetButtonClickHandler(&buttonB, HandleButtonClickB);
+        SimulateButtonClick(&buttonB);
+    #endif
+    // buttonB click
+    #if MY_DEFINE == 2
+        // Create an event
+        
+        Event myEvent;
 
     // Set the click handler for the event
-    SetClickHandler(&myEvent, HandleClick);
+        SetClickHandler(&myEvent, HandleClick);
 
     // Set the double click handler for the event (new functionality)
-    SetDoubleClickHandler(&myEvent, HandleDoubleClick);
+        SetDoubleClickHandler(&myEvent, HandleDoubleClick);
 
     // Simulate a single click
-    SimulateClick(&myEvent);
+        SimulateClick(&myEvent);
 
     // Simulate a double click (new functionality)
-    SimulateDoubleClick(&myEvent);
-
+        SimulateDoubleClick(&myEvent);
+    #endif
     return 0;
 }
 
 
 
-//======================================================================================================
-#include <stdio.h>
+// //======================================================================================================
+// #include <stdio.h>
 
-// Biến toàn cục (Không nên sử dụng)
-int globalVariable = 10;
+// // Biến toàn cục (Không nên sử dụng)
+// int globalVariable = 10;
 
-// Hàm truy cập thay vì trực tiếp sử dụng biến toàn cục
-int getGlobalVariable() {
-    return globalVariable;
-}
+// // Hàm truy cập thay vì trực tiếp sử dụng biến toàn cục
+// int getGlobalVariable() {
+//     return globalVariable;
+// }
 
-void setGlobalVariable(int value) {
-    globalVariable = value;
-}
+// void setGlobalVariable(int value) {
+//     globalVariable = value;
+// }
 
-int main() {
-    printf("Initial Global Variable: %d\n", getGlobalVariable());
+// int main() {
+//     printf("Initial Global Variable: %d\n", getGlobalVariable());
 
-    // Thay đổi giá trị bằng hàm truy cập
-    setGlobalVariable(20);
+//     // Thay đổi giá trị bằng hàm truy cập
+//     setGlobalVariable(20);
 
-    // In giá trị mới bằng hàm truy cập
-    printf("Modified Global Variable: %d\n", getGlobalVariable());
+//     // In giá trị mới bằng hàm truy cập
+//     printf("Modified Global Variable: %d\n", getGlobalVariable());
 
-    return 0;
-}
+//     return 0;
+// }
 
 
-//========================================== const pointer =================================
+// //========================================== const pointer =================================
